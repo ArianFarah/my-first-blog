dc1255246497a9485f24b311780dfffa401125ca
ArianFarah <af.farah44@gmail.com>
2025-01-16 03:12:44 -0800

    Added views to create/edit blog post inside the site.

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,5 @@ from .models import Post
 
 admin.site.register(Post)
 
+#user name , password : nubesag17
+#اگر دفعه بعدی که کد را اجرا کردی و وارد سایت شدی گزینه های ایجاد پست جدید و ادیت پست های فعلی وجود نداشن کافیه دوباره رمز و بزنی و وارد پنل ادمین بشی.
